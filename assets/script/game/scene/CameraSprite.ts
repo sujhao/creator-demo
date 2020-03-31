b82c8d979e40213a023fe84a5c8c3dde1d3fe36d
sujiehao <sujiehao@work.com>
2020-03-31 19:05:13 +0800

    fix

--- assets/script/game/scene/CameraSprite.ts
+++ assets/script/game/scene/CameraSprite.ts
@@ -8,7 +8,7 @@ const MINI_CAMERA_Z = 100;
 
 
 @ccclass
-export default class CameraTest extends cc.Component {
+export default class CameraSprite extends cc.Component {
 
     @property(cc.Node)
     private target: cc.Node = null;
@@ -21,6 +21,11 @@ export default class CameraTest extends cc.Component {
     @property(cc.Graphics)
     private cameraInfo: cc.Graphics = null;
 
+    
+    @property(cc.Sprite)
+    renderTextureSprite: cc.Sprite = null;
+
+
     private cameraPos: cc.Vec3 = cc.v3(0, 0, MINI_CAMERA_Z);
     private cameraOrthoSize: number = Math.ceil(295/2);
 
@@ -28,8 +33,10 @@ export default class CameraTest extends cc.Component {
 
 
 
+    private _renderTexture: cc.RenderTexture = new cc.RenderTexture();
 
     start() {
+
         cc.debug.setDisplayStats(false);
         // let t = cc.tween(this.target)
         //     .by(6, { angle: 360 })
@@ -53,6 +60,33 @@ export default class CameraTest extends cc.Component {
         this.cameraPos = cc.v3(0, 0, MINI_CAMERA_Z);
 
         this.node.on(cc.Node.EventType.TOUCH_MOVE, this.onNodeIconTouchMove, this);
+        this.initRenderTexture();
+    }
+    // onEnable () {
+    //     cc.view.on('design-resolution-changed', this._delayInitRenderTexture, this);
+    // }
+    // _delayInitRenderTexture () {
+    //     // should calculate size after canvas updated
+    //     this.scheduleOnce(this.initRenderTexture.bind(this), 0.1);
+    // }
+
+    private initRenderTexture () {
+        let { width: canvasWidth, height: canvasHeight } = cc.Canvas.instance.node;
+        let cameraWidth: number = this.miniMapCamera.rect.width * canvasWidth;
+        let cameraHeight: number = this.miniMapCamera.rect.height * canvasHeight;
+        Logger.log("initRenderTexture==",this.miniMapCamera.rect.width, this.miniMapCamera.rect.height, cameraWidth, cameraHeight)
+        // this._renderTexture.initWithSize(cameraWidth, cameraHeight);
+        let tempWidth:number = 500;
+        let tempHeight:number = 295;
+        this._renderTexture.initWithSize(tempWidth,tempHeight);
+        this.miniMapCamera.targetTexture = this._renderTexture;
+
+        let spriteFrame = new cc.SpriteFrame();
+        spriteFrame.setTexture(this._renderTexture);
+        this.renderTextureSprite.spriteFrame = spriteFrame;
+        this.renderTextureSprite.node.width = tempWidth;
+        this.renderTextureSprite.node.height = tempHeight;
+        
     }
 
     private onNodeIconTouchMove(evt: cc.Event.EventTouch) {
@@ -61,9 +95,7 @@ export default class CameraTest extends cc.Component {
     }
 
 
-
     update() {
-
 
         this.cameraPos = cc.v3(this.target.x, this.target.y, MINI_CAMERA_Z)
         let { width: canvasWidth, height: canvasHeight } = cc.Canvas.instance.node;
@@ -77,22 +109,29 @@ export default class CameraTest extends cc.Component {
         let cameraWidth: number = this.miniMapCamera.rect.width * deviceWidth;
         let afd = 1 - (deviceWidth / 2 - this.cameraPos.x + cameraWidth / 2) / deviceWidth;
 
-        this.miniMapCamera.rect.x = afd
-        let rect = this.miniMapCamera.rect;
-        this.miniMapCamera.rect = cc.rect(afd, rect.y, rect.width,rect.height)
+        // Logger.log("afd==", afd, cameraWidth);
+        // this.miniMapCamera.rect.x = afd
+        // let rect = this.miniMapCamera.rect;
+        // this.miniMapCamera.rect = cc.rect(afd, rect.y, rect.width,rect.height)
 
         this.cameraInfo.clear();
         // Logger.log("update=", this.cameraOrthoSize, orthoWidth, orthoHeight, rect, this.cameraPos)
         // draw mini camera border
         // draw mini camera border
-        let yellowX: number = (rect.x - 0.5) * deviceWidth;
-        let yellowY: number = (rect.y - 0.5) * deviceHeight;
+        // let yellowX: number = (rect.x - 0.5) * deviceWidth;
+        // let yellowY: number = (rect.y - 0.5) * deviceHeight;
         // Logger.log("update=", deviceWidth, deviceHeight, rect, yellowX, yellowY, rect.width * deviceWidth, rect.height * deviceHeight)
 
 
-        this.cameraInfo.rect(yellowX, yellowY, rect.width * deviceWidth, rect.height * deviceHeight);
+        // this.cameraInfo.rect(yellowX, yellowY, rect.width * deviceWidth, rect.height * deviceHeight);
+        // this.cameraInfo.strokeColor = cc.Color.YELLOW;
+        // this.cameraInfo.stroke();
+
+        let renderTextureNode = this.renderTextureSprite.node;
+        this.cameraInfo.rect(renderTextureNode.x - renderTextureNode.width/2, renderTextureNode.y - renderTextureNode.height/2, renderTextureNode.width, renderTextureNode.height);
         this.cameraInfo.strokeColor = cc.Color.YELLOW;
         this.cameraInfo.stroke();
+
 
         // Logger.log("update=", this.cameraPos, orthoWidth, orthoHeight)
         // draw mini camera ortho size
@@ -103,8 +142,6 @@ export default class CameraTest extends cc.Component {
 
         this.miniMapCamera.node.position = this.cameraPos;
         this.miniMapCamera.orthoSize = this.cameraOrthoSize;
-
-        Logger.log("camnera==", this.cameraPos, this.cameraOrthoSize);
     }
 
     onDestroy() {
