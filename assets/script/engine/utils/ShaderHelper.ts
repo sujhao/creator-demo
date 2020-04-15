9a73f744b61dd46c4f84c0f0b16cfec892117197
sujiehao <sujiehao@work.com>
2020-04-15 14:53:39 +0800

    更改shader使用方式，加入MaterialVariant

--- assets/script/engine/utils/ShaderHelper.ts
+++ assets/script/engine/utils/ShaderHelper.ts
@@ -28,13 +28,15 @@ export default class ShaderHelper {
      */
     public static setGrayEffect(showNode: cc.Node, grayLevel: number = 1, material: cc.Material = ShaderMaterialPrefab.instance.getComponent(ShaderMaterialPrefab).grayMaterial) {
         showNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-            material.setProperty("grayLevel", grayLevel);
-            renderComponent.setMaterial(0, material)
+            let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+            materialVariant.setProperty("grayLevel", grayLevel);
+            renderComponent.setMaterial(0, materialVariant)
         });
         showNode.children.forEach(childNode => {
             childNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-                material.setProperty("grayLevel", grayLevel);
-                renderComponent.setMaterial(0, material)
+                let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+                materialVariant.setProperty("grayLevel", grayLevel);
+                renderComponent.setMaterial(0, materialVariant)
             });
         });
     }
@@ -66,13 +68,15 @@ export default class ShaderHelper {
      */
     public static setOldPhotoEffect(showNode: cc.Node, grayLevel: number = 1, material: cc.Material = ShaderMaterialPrefab.instance.getComponent(ShaderMaterialPrefab).oldPhoto) {
         showNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-            material.setProperty("oldLevel", grayLevel);
-            renderComponent.setMaterial(0, material)
+            let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+            materialVariant.setProperty("oldLevel", grayLevel);
+            renderComponent.setMaterial(0, materialVariant)
         });
         showNode.children.forEach(childNode => {
             childNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-                material.setProperty("oldLevel", grayLevel);
-                renderComponent.setMaterial(0, material)
+                let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+                materialVariant.setProperty("oldLevel", grayLevel);
+                renderComponent.setMaterial(0, materialVariant)
             });
         });
     }
@@ -107,17 +111,19 @@ export default class ShaderHelper {
     */
     public static setGlowInner(showNode: cc.Node, materialParam: any, material: cc.Material = ShaderMaterialPrefab.instance.getComponent(ShaderMaterialPrefab).glowInner) {
         showNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-            material.setProperty("glowColor", materialParam.glowColor);
-            material.setProperty("glowColorSize", materialParam.glowColorSize);
-            material.setProperty("glowThreshold", materialParam.glowThreshold);
-            renderComponent.setMaterial(0, material)
+            let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+            materialVariant.setProperty("glowColor", materialParam.glowColor);
+            materialVariant.setProperty("glowColorSize", materialParam.glowColorSize);
+            materialVariant.setProperty("glowThreshold", materialParam.glowThreshold);
+            renderComponent.setMaterial(0, materialVariant)
         });
         showNode.children.forEach(childNode => {
             childNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-                material.setProperty("glowColor", materialParam.glowColor);
-                material.setProperty("glowColorSize", materialParam.glowColorSize);
-                material.setProperty("glowThreshold", materialParam.glowThreshold);
-                renderComponent.setMaterial(0, material)
+                let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+                materialVariant.setProperty("glowColor", materialParam.glowColor);
+                materialVariant.setProperty("glowColorSize", materialParam.glowColorSize);
+                materialVariant.setProperty("glowThreshold", materialParam.glowThreshold);
+                renderComponent.setMaterial(0, materialVariant)
             });
         });
     }
@@ -132,12 +138,32 @@ export default class ShaderHelper {
     }
 
 
+
     /**
      * 播放被攻击闪烁过程动画
      */
-    public static showFlash(showNode: cc.Node, totalFlashTimes: number = 1) {
+    public static showGreenHatFlash(showNode: cc.Node, callback: Function, totalFlashTimes: number = 1, color: cc.Color = cc.Color.GREEN) {
         let timeCount: number = 0;
-        let color: cc.Color = cc.Color.WHITE;
+        let intervalId = setInterval(() => {
+            timeCount += 1;
+            // let value:number = timeCount*0.01;
+            let greenValue:number =  0.08 * timeCount + 0.05;
+            Logger.log("showGreenHatFlash====", timeCount, greenValue)
+            ShaderHelper.setGlowInner(showNode, { "glowColor": cc.Color.GREEN, "glowColorSize":greenValue, "glowThreshold": 0.1 });
+            if(timeCount >= 12){
+                clearInterval(intervalId);
+                callback && callback();
+            }
+        }, 2000/12)
+    }
+
+
+    /**
+     * 播放被攻击闪烁过程动画
+     */
+    public static showFlash(showNode: cc.Node, totalFlashTimes: number = 1, color: cc.Color = cc.Color.GREEN) {
+        let timeCount: number = 0;
+        // let color: cc.Color = cc.Color.WHITE;
         let flashTimes: number = 0;
         let intervalId = setInterval(() => {
             timeCount += 1;
@@ -165,15 +191,17 @@ export default class ShaderHelper {
      */
     public static setMosaic(showNode: cc.Node, materialParam: any, material: cc.Material = ShaderMaterialPrefab.instance.getComponent(ShaderMaterialPrefab).mosaic) {
         showNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-            material.setProperty("xBlockCount", materialParam.xBlockCount);
-            material.setProperty("yBlockCount", materialParam.yBlockCount);
-            renderComponent.setMaterial(0, material)
+            let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+            materialVariant.setProperty("xBlockCount", materialParam.xBlockCount);
+            materialVariant.setProperty("yBlockCount", materialParam.yBlockCount);
+            renderComponent.setMaterial(0, materialVariant)
         });
         showNode.children.forEach(childNode => {
             childNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-                material.setProperty("xBlockCount", materialParam.xBlockCount);
-                material.setProperty("yBlockCount", materialParam.yBlockCount);
-                renderComponent.setMaterial(0, material)
+                let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+                materialVariant.setProperty("xBlockCount", materialParam.xBlockCount);
+                materialVariant.setProperty("yBlockCount", materialParam.yBlockCount);
+                renderComponent.setMaterial(0, materialVariant)
             });
         });
     }
@@ -183,17 +211,17 @@ export default class ShaderHelper {
    * 播放被攻击闪烁过程动画
    */
     public static showMosaicMv(showNode: cc.Node, callback: Function = null) {
-        let masaicTimes: number = 500;
+        let masaicTimes: number = 50;
         let intervalId = setInterval(() => {
             masaicTimes -= 2;
             this.setMosaic(showNode, { "xBlockCount": masaicTimes, "yBlockCount": masaicTimes })
-            if (masaicTimes <= 30) {
+            if (masaicTimes <= 0) {
                 clearInterval(intervalId)
                 if (callback) {
                     callback();
                 }
             }
-        }, 1)
+        }, 80)
     }
 
     /**
@@ -203,17 +231,17 @@ export default class ShaderHelper {
      */
     public static setRoundCornerCrop(showNode: cc.Node, roundCornerRadius: number = 0.1, material: cc.Material = ShaderMaterialPrefab.instance.getComponent(ShaderMaterialPrefab).roundCornerCrop) {
         showNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-            // material.setProperty("roundCornerRadius", roundCornerRadius);
-            material.setProperty("xRadius", roundCornerRadius);
-            material.setProperty("yRadius", roundCornerRadius);
-            renderComponent.setMaterial(0, material)
+            let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+            materialVariant.setProperty("xRadius", roundCornerRadius);
+            materialVariant.setProperty("yRadius", roundCornerRadius);
+            renderComponent.setMaterial(0, materialVariant)
         });
         showNode.children.forEach(childNode => {
             childNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-                // material.setProperty("roundCornerRadius", roundCornerRadius);
-                material.setProperty("xRadius", roundCornerRadius);
-                material.setProperty("yRadius", roundCornerRadius);
-                renderComponent.setMaterial(0, material)
+                let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+                materialVariant.setProperty("xRadius", roundCornerRadius);
+                materialVariant.setProperty("yRadius", roundCornerRadius);
+                renderComponent.setMaterial(0, materialVariant)
             });
         });
     }
@@ -231,23 +259,25 @@ export default class ShaderHelper {
      */
     public static setFlashLight(showNode: cc.Node, lightColor: cc.Color, lightWidth: number, lightAngle: number = 0, enableGradient: boolean = true, cropAlpha: boolean = true, enableFog: boolean = false, material: cc.Material = ShaderMaterialPrefab.instance.getComponent(ShaderMaterialPrefab).flashLight) {
         showNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-            material.setProperty("lightColor", lightColor);
-            material.setProperty("lightWidth", lightWidth);
-            material.setProperty("lightAngle", lightAngle);
-            material.setProperty("enableGradient", enableGradient);
-            material.setProperty("cropAlpha", cropAlpha);
-            material.setProperty("enableFog", enableFog);
-            renderComponent.setMaterial(0, material)
+            let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+            materialVariant.setProperty("lightColor", lightColor);
+            materialVariant.setProperty("lightWidth", lightWidth);
+            materialVariant.setProperty("lightAngle", lightAngle);
+            materialVariant.setProperty("enableGradient", enableGradient);
+            materialVariant.setProperty("cropAlpha", cropAlpha);
+            materialVariant.setProperty("enableFog", enableFog);
+            renderComponent.setMaterial(0, materialVariant)
         });
         showNode.children.forEach(childNode => {
             childNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-                material.setProperty("lightColor", lightColor);
-                material.setProperty("lightWidth", lightWidth);
-                material.setProperty("lightAngle", lightAngle);
-                material.setProperty("enableGradient", enableGradient);
-                material.setProperty("cropAlpha", cropAlpha);
-                material.setProperty("enableFog", enableFog);
-                renderComponent.setMaterial(0, material)
+                let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+                materialVariant.setProperty("lightColor", lightColor);
+                materialVariant.setProperty("lightWidth", lightWidth);
+                materialVariant.setProperty("lightAngle", lightAngle);
+                materialVariant.setProperty("enableGradient", enableGradient);
+                materialVariant.setProperty("cropAlpha", cropAlpha);
+                materialVariant.setProperty("enableFog", enableFog);
+                renderComponent.setMaterial(0, materialVariant)
             });
         });
     }
@@ -307,14 +337,15 @@ export default class ShaderHelper {
      */
     public static setGaussian(showNode: cc.Node, material: cc.Material = ShaderMaterialPrefab.instance.getComponent(ShaderMaterialPrefab).gaussian) {
         showNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-            material.setProperty("textureSize", cc.v2(renderComponent.node.width, renderComponent.node.height));
-            renderComponent.setMaterial(0, material)
+            let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+            materialVariant.setProperty("textureSize", cc.v2(renderComponent.node.width, renderComponent.node.height));
+            renderComponent.setMaterial(0, materialVariant)
         });
         showNode.children.forEach(childNode => {
             childNode.getComponents(cc.RenderComponent).forEach((renderComponent: cc.RenderComponent) => {
-                material.setProperty("textureSize", cc.v2(renderComponent.node.width, renderComponent.node.height));
-                // material.setProperty("textureSize", cc.v2(showNode.width, showNode.height));
-                renderComponent.setMaterial(0, material)
+                let materialVariant:cc.MaterialVariant = cc.MaterialVariant.create(material, renderComponent);
+                materialVariant.setProperty("textureSize", cc.v2(renderComponent.node.width, renderComponent.node.height));
+                renderComponent.setMaterial(0, materialVariant)
             });
         });
     }
